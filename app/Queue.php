c7a6a211031ffe6e0c33a10f803fc7d36da81da3
Vito Famiglietti <vito.famiglietti@gmail.com>
2017-10-11 18:13:30 +0200

    Queue model

---
+++ app/Queue.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Queue extends Model
+{
+    protected $fillable = [
+        'type',
+        'status',
+        'needed_stations',
+    ];
+
+    public function players()
+    {
+        return $this->hasMany(App\Player::class);
+    }
+}
