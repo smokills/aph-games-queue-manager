f1e3cc107fa79ca964d2cd8fb6260bc8a5cc72e1
Vito Famiglietti <vito.famiglietti@gmail.com>
2017-10-11 18:13:49 +0200

    players table migration

---
+++ database/migrations/2017_10_11_100230_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ign');
+            $table->integer('queue_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('queue_id')
+                    ->references('id')
+                    ->on('queues')
+                    ->onDelete('Cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
