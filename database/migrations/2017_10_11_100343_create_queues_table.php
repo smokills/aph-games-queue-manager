fb34ea0d46e1de0aa30212fcfb28be84a9b73c1f
Vito Famiglietti <vito.famiglietti@gmail.com>
2017-10-11 18:14:19 +0200

    queues table migration

---
+++ database/migrations/2017_10_11_100343_create_queues_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQueuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->enum('waiting', 'processing', 'idle')->default('waiting');
+            $table->integer('needed_stations')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('queues');
+    }
+}
