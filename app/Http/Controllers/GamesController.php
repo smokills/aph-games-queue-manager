7a2764b4e7601e6631d675e7fc18b3a732801053
Vito Famiglietti <vito.famiglietti@gmail.com>
2017-10-20 17:54:19 +0200

    added GamesController

---
+++ app/Http/Controllers/GamesController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Game;
+use Illuminate\Http\Request;
+
+class GamesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('games.index', ['games' => Game::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('games.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate($this->validationRules());
+
+        $game = Game::create($data);
+
+        flash("Game {$game->name} added!")->success();
+
+        return redirect(route('games.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Game $game)
+    {
+        return view('games.show', ['game' => $game]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Game $game)
+    {
+        return view('games.edit', ['game' => $game]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Game $game)
+    {
+        $data = $request->validate($this->validationRules());
+
+        $game->update($data);
+
+        flash("Game {$game->name} updated!")->info();
+
+        return redirect(route('games.show', ['game' => $game->id]));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Game $game)
+    {
+        $game->delete();
+
+        flash("Game deleted")->info();
+
+        return redirect(route('games.index'));
+    }
+
+    protected function validationRules()
+    {
+        return [
+            'name' => 'required|string|max:20'
+        ];
+    }
+}
